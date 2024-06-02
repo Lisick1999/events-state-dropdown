8e935e7e49c133a13dee026e47bc2b69684fcf5b
Fox Say <Lisick1999@yandex.ru>
2024-06-02 19:42:29 +0300

    initial commit

---
+++ src/components/ListView.jsx
@@ -0,0 +1,17 @@
+import ShopItem from "./ShopItem";
+
+
+export default function ListView({ items }) {
+  return (
+    <div className="items">
+      {items.map(item =>
+        <ShopItem
+          key={item.img + item.price}
+          name={item.name}
+          color={item.color}
+          img={item.img}
+          price={item.price}
+        />)}
+    </div>
+  )
+}
---
+++ src/components/ShopItem.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button"
+
+export default function ShopItem({name, color, img, price}) {
+    
+  return (
+      <div className = "item">
+      <div><img src = {img} alt = {name} className ="imgItem"/></div>
+      <h3>{name}</h3>
+      <div>{color}</div>
+      <div className = "price">${price}</div>
+      <Button/>
+    </div>
+  )
+}
